d07db8fa127825b92356f7e424d83aa00ff9244d
Wellington Moreira Ramos <wmaramos@gmail.com>
2020-10-22 03:06:42 -0300

    Add localstack

--- examples/s3-complete/main.tf
+++ examples/s3-complete/main.tf
@@ -1,5 +1,16 @@
 provider "aws" {
   region = var.region
+
+  # localstack settings
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  s3_force_path_style         = true
+  endpoints {
+    s3 = "http://localstack:4566"
+  }
 }
 
 module "s3" {
